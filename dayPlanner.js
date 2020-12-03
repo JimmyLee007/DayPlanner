1c25f60a2c4f5a28470d1b478f1812a1f9607a6a
Jimmy Lee <jamesworld2007@hotmail.co.uk>
2020-12-03 15:01:41 +0000

    Add functionality to randomly select the differnt message comonents

--- dayPlanner.js
+++ dayPlanner.js
@@ -1,5 +1,18 @@
+// Data structure to hold message components
 const messageComponents = {
     excercise : ['Jogging', 'Cycling', 'Dancing', 'Rowing', 'Swimming', 'Body weight excercises'],
     meal : ['Porridge', 'Bread', 'Veg', 'Salad', 'Rice', 'Fruit'],
     relaxation : ['Watch a film', 'read a book', 'play computer game', 'surf the internet']
 };
+
+// returning an array of the randomly selected components
+function selectMessageComponents(object){
+let selectedMessageComponents = [];
+for(key in object){
+    let indexNumber = Math.floor(Math.random() * object[key].length);
+    selectedMessageComponents.push(object[key][indexNumber]);
+};
+return selectedMessageComponents;
+}
+
+console.log(selectMessageComponents(messageComponents));
