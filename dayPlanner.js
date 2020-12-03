c677f9b671c8f49d06393c740b4f4c5afabdfad2
Jimmy Lee <jamesworld2007@hotmail.co.uk>
2020-12-03 14:22:43 +0000

    store message components in a data structure

--- dayPlanner.js
+++ dayPlanner.js
@@ -1,1 +1,5 @@
-console.log('test script output');
+const messageComponents = {
+    excercise : ['Jogging', 'Cycling', 'Dancing', 'Rowing', 'Swimming', 'Body weight excercises'],
+    meal : ['Porridge', 'Bread', 'Veg', 'Salad', 'Rice', 'Fruit'],
+    relaxation : ['Watch a film', 'read a book', 'play computer game', 'surf the internet']
+};
